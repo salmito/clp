fa0b4682437bea0958ae39580c723cba6cf24cd3
Tiago Salmito <tiago@salmito.com>
2014-03-25 20:29:00 -0300

    added timer test

---
+++ test/timer.lua
@@ -0,0 +1,25 @@
+local lstage=require'lstage'
+
+
+local function add_timer(time,f)
+	return lstage.stage(function()
+	 	while true do
+			lstage.event.sleep(time)
+			f()		
+		end
+	end):push()
+end
+
+local id=1
+function handler(id) return function()
+		thn=thn or lstage.now()
+		print(require'string'.format("id %d slept for %.6f",id,lstage.now()-thn))
+		thn=lstage.now()
+	end
+end
+
+add_timer(0.1,handler(1))
+add_timer(0.4,handler(2))
+add_timer(0.8,handler(3))
+
+lstage.channel():get() --dorme para sempre
