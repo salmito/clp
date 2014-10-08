26b8a8f701ee8f2ba800499363509d096dba4a00
Tiago Salmito <tiago@salmito.com>
2014-10-07 21:11:35 -0300

    better event handling

--- src/clp.c
+++ src/clp.c
@@ -8,10 +8,6 @@
 #include "pool.h"
 #include "threading.h"
 
-#ifdef _WIN32
-#define sleep(a) Sleep(a * 1000)
-#else
-
 #ifdef DEBUG
 //can be found here  http://www.lua.org/pil/24.2.3.html
 void stackDump (lua_State *L, const char *text) {
