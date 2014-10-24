48f5129461286fd93615c01b2aafa2fbd23c9f83
Tiago Salmito <tiago@salmito.com>
2014-10-23 22:00:13 -0200

    updated again

--- src/mingw64.sh
+++ src/mingw64.sh
@@ -13,7 +13,7 @@ make ultraclean
 
 make -j _SO=".dll" CC=i686-w64-mingw32-gcc G_PLUS_PLUS=i686-w64-mingw32-g++ EXTRA_FLAGS="-I/usr/include/event2 -I/usr/include/tbb" LDFLAGS="dll/x32/a/libevent.a dll/x32/a/libevent_core.a dll/x32/a/libevent_extra.a -Ldll/x32/ -lwsock32 -lws2_32 -ltbb32 -lliblua" LUA_VER=5.1
 mkdir -p dll/x32/lua51/
-cp clp.dll dll/x32/lua51/clp.dll
+cp clp.dll dll/x32/lua51/clp-liblua.dll
 make ultraclean
 
 make -j _SO=".dll" CC=i686-w64-mingw32-gcc G_PLUS_PLUS=i686-w64-mingw32-g++ EXTRA_FLAGS="-I/usr/include/event2 -I/usr/include/tbb" LDFLAGS="dll/x32/a/libevent.a dll/x32/a/libevent_core.a dll/x32/a/libevent_extra.a -Ldll/x32/ -lwsock32 -lws2_32 -ltbb32 -llua51" LUA_VER=5.1
